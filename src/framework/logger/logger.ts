e63d7febd3a63f11ff421f01772f9f3e0a775091
Rrayor <simon.benjamin93@gmail.com>
2021-03-25 23:22:36 +0100

    changed ~ to @ in paths

--- src/framework/logger/logger.ts
+++ src/framework/logger/logger.ts
@@ -1,4 +1,4 @@
-import { LogTarget } from '~/framework/logger/enums/log-target.enum';
+import { LogTarget } from '@framework/logger/enums/log-target.enum';
 
 import { LogLevel } from './enums/log-level.enum';
 
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Logger, LogLevel, LogTarget } from '~/framework/logger';
+import { Logger, LogLevel, LogTarget } from '@framework/logger';
 
 const logger = new Logger(LogTarget.CONSOLE, 'LOGGER_1');
 logger.log('Hello Logging!', LogLevel.WARN);
