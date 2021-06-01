15fa08edbce50f3c0761ab670a87e38c0d81d5a1
Rrayor <simon.benjamin93@gmail.com>
2021-06-01 19:52:39 +0200

    pixi level loader plugin

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.(png|svg|jpg|jpeg|gif|json)$/i,
+                test: /\.(png|svg|jpg|jpeg|gif|level)$/i,
                 type: 'asset/resource',
             },
             {
