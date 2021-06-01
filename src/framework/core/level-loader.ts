a28fe340639f43e0ec6ae0a55a89c466ae6fb982
Rrayor <simon.benjamin93@gmail.com>
2021-06-01 20:36:47 +0200

    worked on json loading

--- src/framework/core/level-loader.ts
+++ src/framework/core/level-loader.ts
@@ -2,22 +2,18 @@ import { ILoaderResource, LoaderResource } from '@pixi/loaders';
 
 import { TileMap } from '../utils/tilemap';
 
-
 const fileExtension = 'level';
 export class LevelLoaderPlugin {
-  static add() {
-    // level files loaded as JSON
-    LoaderResource.setExtensionXhrType(
-      fileExtension,
-      LoaderResource.XHR_RESPONSE_TYPE.JSON
-    );
-  }
-  static use(resource: ILoaderResource, next: () => void) {
-    if (resource.extension !== fileExtension) {
-      return next();
+    static add() {
+        // level files loaded as JSON
+        LoaderResource.setExtensionXhrType(fileExtension, LoaderResource.XHR_RESPONSE_TYPE.JSON);
     }
-    const level = new TileMap(resource.data.width, resource.data.height, resource.data.tileSize, resource.data.layers, resource.data.tileConfig);
-    resource.data = level;
-    next();
-    };
-  }
+    static use(resource: ILoaderResource, next: (p?: any) => any) {
+        if (resource.extension !== fileExtension) {
+            return next();
+        }
+        const level = new TileMap(resource.data.width, resource.data.height, resource.data.tileSize, resource.data.layers, resource.data.tileConfig);
+        resource.data = level;
+        next();
+    }
+}
