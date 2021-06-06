78035972445d44350db6d012723b3cb9a6ff5746
Rrayor <simon.benjamin93@gmail.com>
2021-06-06 22:44:16 +0200

    added ngx-translate

--- ts-pixi-ui/src/app/app.component.ts
+++ ts-pixi-ui/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 import { runGame } from '../example-game/run-game';
 
 @Component({
@@ -7,6 +8,14 @@ import { runGame } from '../example-game/run-game';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+
+  constructor(protected translateService: TranslateService) {
+    // this language will be used as a fallback when a translation isn't found in the current language
+    translateService.setDefaultLang('en-US');
+
+    // the lang to use, if the lang isn't available, it will use the current loader to get them
+    translateService.use('en-US');
+  }
 
   ngOnInit(): void {
     runGame();
--- ts-pixi-ui/src/app/app.module.ts
+++ ts-pixi-ui/src/app/app.module.ts
@@ -2,15 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    // TODO: May be exported from a shared module without a forRoot() call
+    TranslateModule.forRoot({
+      defaultLanguage: 'en-US',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [HttpClient]
+      }
+    })
   ],
-  providers: [],
+  providers: [
+    TranslateService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
